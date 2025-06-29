4ae4746a92192a9b1744be56425ec1f837fa0f36
Dante Souza e Souza <lokinmodar@gmail.com>
2025-06-29 00:34:02 -0300

    feat: new translators tweaks

--- Translators/DeepSeek/DeepSeekTextModelDefaults.cs
+++ Translators/DeepSeek/DeepSeekTextModelDefaults.cs
@@ -1,10 +1,13 @@
-﻿namespace Echoglossian.Translators.DeepSeek
+﻿using Echoglossian.Translators.OpenAI;
+
+namespace Echoglossian.Translators.DeepSeek
 {
   public static class DeepSeekTextModelDefaults
   {
     public static readonly List<OpenAITextModel> PredefinedModels = new()
     {
         new("deepseek-chat", "DeepSeek Chat", true, false, true, false),
-        new("deepseek-reasoner", "DeepSeek Reasoner", true, false, false, false)
+        new("deepseek-reasoner", "DeepSeek Reasoner", true, false, false, false),
     };
   }
+}
